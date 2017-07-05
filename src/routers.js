94482d4c8abe2fee5545c5fa6280a628042e4da1
fangyang <fangyang@ffcs.cn>
2017-07-05 14:45:11 +0800

    test

--- src/routers.js
+++ src/routers.js
@@ -1,17 +1,17 @@
-import HelloFromVux from './components/HelloFromVux'
-import Detail from './components/Detail'
+import Repository from './components/Repository'
+import RepositoryDetail from './components/RepositoryDetail'
 
 
 const routers = [
 
   {
     path: '/',
-    component: HelloFromVux,
+    component: Repository,
      meta:{title:'知识库'}
   },
   {
-    path: '/detail',
-    component: Detail,
+    path: '/repositoryDetail',
+    component: RepositoryDetail,
     meta:{title:'详情'}
   }
 ]
