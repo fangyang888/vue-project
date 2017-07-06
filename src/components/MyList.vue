d67e75637ce06b48f36bd05a8111b7c6eeb63a16
fangyang <fangyang@ffcs.cn>
2017-07-06 18:00:20 +0800

    test

---
+++ src/components/MyList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <list-component :list="list" :searchkey="searchkey"  :columns="columns" :mydata="mydata"></list-component>
+    </div>
+</template>
+
+<script>
+    import ListComponent from './ListComponent'
+    export default {
+        components: {
+            'ListComponent':ListComponent
+        },
+        data () {
+            return {
+              list:[{name:"zhangsh",age:12,time:'2017-10-12'},{name:"zhangsh",age:14,time:'2017-11-12'}],
+              searchkey:'',
+              columns:['name','age','time'],
+              mydata:{"name":"hash","age":123}
+
+            }
+        },
+        computed: {
+
+        },
+        created(){
+
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
